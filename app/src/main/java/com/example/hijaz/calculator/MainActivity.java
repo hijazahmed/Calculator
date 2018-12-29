5288402dcaef50f6312733b2fe227b4d40019bf1
hijazahmed <44543188+hijazahmed@users.noreply.github.com>
2018-12-29 12:38:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hijaz/calculator/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.hijaz.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
+
+    //variables to hold operands and calulations
+
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        result = (EditText) findViewById(R.id.result);
+        newNumber = (EditText) findViewById(R.id.newNumber);
+        displayOperation = (TextView) findViewById(R.id.displayOperation);
+
+
+        Button button0 = (Button) findViewById(R.id.button0);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+        Button buttonDot = (Button) findViewById(R.id.buttonDot);
+
+        Button buttonEqualls = (Button) findViewById(R.id.buttonEqualls);
+        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
+
+        View.OnClickListener listner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                newNumber.append(b.getText().toString());
+            }
+        };
+
+        button0.setOnClickListener(listner);
+        button1.setOnClickListener(listner);
+        button2.setOnClickListener(listner);
+        button3.setOnClickListener(listner);
+        button4.setOnClickListener(listner);
+        button5.setOnClickListener(listner);
+        button6.setOnClickListener(listner);
+        button7.setOnClickListener(listner);
+        button8.setOnClickListener(listner);
+        button9.setOnClickListener(listner);
+        buttonDot.setOnClickListener(listner);
+
+        View.OnClickListener opListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                String op = b.getText().toString();
+                String value = newNumber.getText().toString();
+              try{
+Double doubleValue = Double.valueOf(value);
+performOperation(doubleValue, op);
+              } catch (NumberFormatException e){
+            newNumber.setText("");
+              }
+                pendingOperation = op;
+                displayOperation.setText(pendingOperation);
+            }
+        };
+        buttonEqualls.setOnClickListener(opListener);
+        buttonDivide.setOnClickListener(opListener);
+        buttonMinus.setOnClickListener(opListener);
+        buttonMultiply.setOnClickListener(opListener);
+        buttonPlus.setOnClickListener(opListener);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    private void performOperation(Double value, String operation) {
+        if (null == operand1) {
+            operand1 =value;
+        } else {
+            operand2 =value;
+
+            if (pendingOperation.equals("=")) {
+
+                pendingOperation = operation;
+            }
+
+            switch (pendingOperation) {
+                case "=" :
+                    operand1 = operand2;
+                    break;
+                case "/":
+                    if(operand2==0){
+                        operand1=0.0;
+                    }else {
+                        operand1 /= operand2;
+                    }
+                    break;
+                case "*":
+                    operand1 *= operand2;
+                    break;
+                case "-":
+                    operand1 -= operand2;
+                    break;
+                case "+":
+                    operand1 +=operand2;
+                    break;
+            }
+
+        }
+                result.setText(operand1.toString());
+        newNumber.setText("");
+    }
+}
